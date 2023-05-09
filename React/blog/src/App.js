aa17dbc730de134aaac284038feb250ae281ea77
Juho Lee <eungseoni@gmail.com>
2023-05-09 19:39:58 +0900

    add: modal function

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -5,10 +5,43 @@ import data from "./data.js";
 
 function App() {
   let [datas, setDatas] = useState(data);
+  let [modal, setModal] = useState(false);
   return (
     <div>
       <div className="black-nav">
         <h3>Daegun High School Subject Introduction</h3>
+      </div>
+      {datas.map(function (row, i) {
+        return (
+          <div className="list">
+            <div
+              className="title"
+              onClick={() => {
+                modal ? setModal(false) : setModal(true);
+              }}
+            >
+              {row.title}
+            </div>
+            <div className="contents">{row.date}</div>
+          </div>
+        );
+      })}
+      {modal ? <Modal></Modal> : ""}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <div className="modal-body">
+        <div>web programming</div>
+        <div>
+          2023-05-09
+          <span>👍</span>
+          <span>5</span>
+        </div>
+        <div>web programming is funny subject</div>
       </div>
     </div>
   );
