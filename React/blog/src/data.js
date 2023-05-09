3f0bf7a9f9ed23a94109d4efbafd1b18877a772f
Juho Lee <eungseoni@gmail.com>
2023-05-09 17:16:05 +0900

    delete: app.js add:data.js

---
+++ React/blog/src/data.js
@@ -0,0 +1,25 @@
+let data = [
+  {
+    id: 0,
+    title: "Web Programming",
+    date: "2023-05-09",
+    content: "subject learn about web programming",
+    like: 5,
+  },
+  {
+    id: 1,
+    title: "Programming",
+    date: "2023-05-09",
+    content: "subject learn about programming in second grade",
+    like: 5,
+  },
+  {
+    id: 2,
+    title: "Informatic",
+    date: "2023-05-09",
+    content: "subject learn about informatic in first grade",
+    like: 3,
+  },
+];
+
+export default data;
