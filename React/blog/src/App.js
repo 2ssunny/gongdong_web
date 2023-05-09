248215db5f6544a56236ab57683a6ec205f6c178
Juho Lee <eungseoni@gmail.com>
2023-05-09 17:57:11 +0900

    feat: add navbar

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,9 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import data from "./data.js";
 
 function App() {
-  return <div>Hello World</div>;
+  let [datas, setDatas] = useState(data);
+  return (
+    <div>
+      <div className="black-nav">
+        <h3>Daegun High School Subject Introduction</h3>
+      </div>
+    </div>
+  );
 }
 
 export default App;
